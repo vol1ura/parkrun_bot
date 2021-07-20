107789b2d434ba2f139c913326ea47b21a37dd75
Yuriy Volodin <vjv@inbox.ru>
2021-07-20 08:09:01 +0300

    add tests for vk module

---
+++ tests/utils/test_vk.py
@@ -0,0 +1,17 @@
+import os
+import re
+
+from utils import vk
+
+
+async def test_get_random_photo():
+    photo_url = await vk.get_random_photo(os.getenv('VK_SERVICE_TOKEN'))
+    print(photo_url)
+    assert isinstance(photo_url, str)
+    assert photo_url.startswith('https://')
+
+
+def test_make_vk_api_url():
+    url = vk.make_vk_api_url('test_token', 'test_method', '-1234567890', '111')
+    assert re.fullmatch(r'https://api\.vk\.com/method/test_method\?owner_id=-1234567890'
+                        r'&album_id=111&access_token=test_token&v=5.130', url)
