3ec8bab1ec0c4ee00ba3f2e785b93cbdad9a5f60
Yuriy Volodin <vjv@inbox.ru>
2021-07-30 08:28:52 +0300

    make class members private

--- parkrun/personal.py
+++ parkrun/personal.py
@@ -9,30 +9,30 @@ from parkrun import parkrun
 
 class PersonalResults:
     def __init__(self, athlete_name, athlete_page):
-        self.athlete_name = athlete_name
-        self.df = parkrun.parse_personal_results(athlete_page)
-        self.df['Год'] = pd.to_datetime(self.df['Дата parkrun'], dayfirst=True).dt.year
-        self.df['Месяц'] = pd.to_datetime(self.df['Дата parkrun'], dayfirst=True) \
+        self.__athlete_name = athlete_name
+        self.__df = parkrun.parse_personal_results(athlete_page)
+        self.__df['Год'] = pd.to_datetime(self.__df['Дата parkrun'], dayfirst=True).dt.year
+        self.__df['Месяц'] = pd.to_datetime(self.__df['Дата parkrun'], dayfirst=True) \
             .dt.month_name(locale='ru_RU.UTF-8').str.slice(stop=3)
-        self.months = ['Янв', 'Фев', 'Мар', 'Апр', 'Мая', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек']
+        self.__months = ['Янв', 'Фев', 'Мар', 'Апр', 'Мая', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек']
 
     def history(self, pic: str):
         fig, ax = plt.subplots(figsize=(9, 6), dpi=150)
-        rundata = self.df.pivot_table(index='Месяц', columns='Год', values='Время', aggfunc=len, fill_value=0) \
+        rundata = self.__df.pivot_table(index='Месяц', columns='Год', values='Время', aggfunc=len, fill_value=0) \
             .astype(int)
 
-        for month in self.months:
+        for month in self.__months:
             if month not in rundata.index.values:
                 rundata.loc[month] = 0
         # sort chronological
-        rundata = rundata.reindex(self.months)
+        rundata = rundata.reindex(self.__months)
         maxruns = rundata.max().max()
         ticks = np.arange(0, maxruns + 1)
         boundaries = np.arange(-0.5, maxruns + 1.5)
 
         sns.heatmap(rundata, linewidths=0.4, cmap='Greens',
                     cbar_kws={"ticks": ticks, "boundaries": boundaries, 'label': 'Паркранов в месяц'})
-        ax.set_title(f'{self.athlete_name}: история участия в паркранах', fontweight='bold')
+        ax.set_title(f'{self.__athlete_name}: история участия в паркранах', fontweight='bold')
         plt.tight_layout()
         plt.savefig(pic)
         return open(pic, 'rb')
@@ -41,17 +41,17 @@ class PersonalResults:
         fig, ax = plt.subplots(figsize=(9, 6), dpi=150)
 
         # pivot df into long form and aggregate by fastest time
-        rundata = self.df.pivot_table(index='Месяц', columns='Год', values='m',
-                                      aggfunc=np.min, fill_value=np.nan)
+        rundata = self.__df.pivot_table(index='Месяц', columns='Год', values='m',
+                                        aggfunc=np.min, fill_value=np.nan)
         # add rows of zeros for any months missed
-        for month in self.months:
+        for month in self.__months:
             if month not in rundata.index.values:
                 rundata.loc[month] = np.nan
         # sort chronological
-        rundata = rundata.reindex(self.months)
+        rundata = rundata.reindex(self.__months)
 
         sns.heatmap(rundata, linewidths=0.4, cmap='hot', cbar_kws={'label': 'Время (минуты)'}, ax=ax)
-        ax.set_title(f'{self.athlete_name}: изменение лучшего результата с годами', fontweight='bold')
+        ax.set_title(f'{self.__athlete_name}: изменение лучшего результата с годами', fontweight='bold')
         plt.tight_layout()
         plt.savefig(pic)
         return open(pic, 'rb')
@@ -59,29 +59,29 @@ class PersonalResults:
     def tourism(self, pic: str):
         fig, ax = plt.subplots(figsize=(9, 6), dpi=150)
         # pivot df into long form and aggregate by fastest time
-        rundata = self.df.pivot_table(index='Месяц', columns='Год', values='Паркран', fill_value=0,
-                                      aggfunc=lambda parkruns: len(np.unique(parkruns, return_counts=True)[1]))
+        rundata = self.__df.pivot_table(index='Месяц', columns='Год', values='Паркран', fill_value=0,
+                                        aggfunc=lambda parkruns: len(np.unique(parkruns, return_counts=True)[1]))
         maxuniq = rundata.max().max()
 
         # add rows of zeros for any months missed
-        for month in self.months:
+        for month in self.__months:
             if month not in rundata.index.values:
                 rundata.loc[month] = np.nan
         # sort chronological
-        rundata = rundata.reindex(self.months)
+        rundata = rundata.reindex(self.__months)
         cmap = sns.cubehelix_palette(rot=-.5, n_colors=maxuniq + 1)
         ticks = np.arange(0, maxuniq + 1)
         boundaries = np.arange(-0.5, maxuniq + 1.5)
         sns.heatmap(rundata, linewidths=0.4, cmap=cmap,
                     cbar_kws={"ticks": ticks, 'label': 'Количество разных паркранов', "boundaries": boundaries}, ax=ax)
-        ax.set_title(f'{self.athlete_name}: интенсивность паркран-туризма', fontweight='bold')
+        ax.set_title(f'{self.__athlete_name}: интенсивность паркран-туризма', fontweight='bold')
         plt.tight_layout()
         plt.savefig(pic)
         return open(pic, 'rb')
 
     def last_runs(self, pic: str):
         plt.figure(figsize=(7, 9), dpi=150)
-        df_last = self.df.head(10)[::-1].reset_index(drop=True)
+        df_last = self.__df.head(10)[::-1].reset_index(drop=True)
         ax = df_last.plot(x='Дата parkrun', y='m', lw=2, label='Результат')
         xlabels = df_last['Дата parkrun']
         ax.set_xticks(xlabels.index)
@@ -102,7 +102,7 @@ class PersonalResults:
         return open(pic, 'rb')
 
     def wins_table(self):
-        pos_df = pd.crosstab(self.df['Паркран'], self.df['Место'], margins=True)
+        pos_df = pd.crosstab(self.__df['Паркран'], self.__df['Место'], margins=True)
         columns = [1, 2, 3, 'All']
         for i in columns:
             if i not in pos_df.columns:
