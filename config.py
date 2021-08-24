fd04613a464e5d89b07891fb99eb016ce8227f86
Yuriy Volodin <vjv@inbox.ru>
2021-08-24 08:02:12 +0300

    fix github action

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-PROJECT_NAME = os.getenv('PROJECT')
-TOKEN_BOT = os.getenv('API_BOT_TOKEN')
+PROJECT_NAME = os.getenv('PROJECT', 'test')
+TOKEN_BOT = os.getenv('API_BOT_TOKEN', '123456:123456test')
 
 WEBHOOK_HOST = f'https://{PROJECT_NAME}.herokuapp.com'
 WEBHOOK_PATH = f'/webhook/{os.getenv("WEBHOOK")}'
@@ -17,4 +17,4 @@ WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = int(os.environ.get('PORT', 5000))
 
 DB_FILE = os.path.join(os.path.dirname(__file__), 'database.vdb')
-REDIS_URL = os.getenv('REDIS_URL')
+REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
