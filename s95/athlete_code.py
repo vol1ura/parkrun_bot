95ebcf5cf4963a84d0953c6e1aa6ca84e7fcde79
Yuriy Volodin <vol1ura@gmail.com>
2023-02-28 08:45:12 +0300

    add parkzhrun conditions to athlete codes

--- s95/athlete_code.py
+++ s95/athlete_code.py
@@ -1,4 +1,5 @@
 class AthleteCode:
+    PARKZHRUN_BORDER = 690_000_000
     SAT_5AM_9KM_BORDER = 770_000_000
     FIVE_VERST_BORDER = 790_000_000
     RUN_PARK_BORDER = 7_000_000_000
@@ -24,6 +25,8 @@ class AthleteCode:
             return 'fiveverst_code'
         if self._is_fiveverst_code:
             return 'fiveverst_code'
+        if self._is_parkzhrun_code:
+            return 'parkzhrun_code'
         return 's95'
 
     @property
@@ -43,14 +46,19 @@ class AthleteCode:
     @property
     def _is_parkrun_code(self) -> bool:
         """1. ParkRun condition"""
-        return self._int_val < self.SAT_5AM_9KM_BORDER
+        return self._int_val < self.PARKZHRUN_BORDER
+
+    @property
+    def _is_parkzhrun_code(self) -> bool:
+        """2. Parkzhrun condition"""
+        return self.PARKZHRUN_BORDER < self._int_val < self.SAT_5AM_9KM_BORDER
 
     @property
     def _is_runpark_code(self) -> bool:
-        """2. RunPark condition"""
+        """3. RunPark condition"""
         return self._int_val > self.RUN_PARK_BORDER
 
     @property
     def _is_fiveverst_code(self) -> bool:
-        """3. 5 verst condition"""
+        """4. 5 verst condition"""
         return self._int_val > self.FIVE_VERST_BORDER
