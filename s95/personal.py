eb6cfcf4a75cb56e73dd1dd60bf6191585cff09c
Yuriy Volodin <vol1ura@gmail.com>
2023-07-21 18:10:14 +0300

    fix diagram title

--- s95/personal.py
+++ s95/personal.py
@@ -105,7 +105,7 @@ class PersonalResults:
         ax.xaxis.set_minor_locator(NullLocator())
         plt.xlabel('Дата забега')
         plt.ylabel('Минуты')
-        plt.title('10 забегов S95', fontweight='bold')
+        plt.title(f'{len(df_last)} забегов S95', fontweight='bold')
         plt.tight_layout()
         plt.savefig(pic)
         return open(pic, 'rb')
