f854299f3dfb9ef150e3f9df52f31c187634ca5d
Yuriy Volodin <vol1ura@gmail.com>
2023-12-07 12:22:16 +0300

    tune qr code

--- utils/qrcode.py
+++ utils/qrcode.py
@@ -7,7 +7,7 @@ from qrcode.image.pure import PyPNGImage
 
 @contextmanager
 def generate(athlete_code: int):
-    code_obj = qrcode.make(f'A{athlete_code}', image_factory=PyPNGImage)
+    code_obj = qrcode.make(f'A{athlete_code}', image_factory=PyPNGImage, box_size=8, border=3)
     qrcode_file = f'gen_png/qrcode_{athlete_code}'
     code_obj.save(qrcode_file)
     file_obj = open(qrcode_file, 'rb')
